d1a90318e5313c46e3147f226906cb51e9b8624c
Derek Waters <derek@frisbeeworld.com>
2018-03-11 23:22:38 +1100

    Format the main list

--- app/src/main/java/com/frisbeeworld/drills/AddSessionActivity.java
+++ app/src/main/java/com/frisbeeworld/drills/AddSessionActivity.java
@@ -1,25 +1,38 @@
 package com.frisbeeworld.drills;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.frisbeeworld.drills.database.Session;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.DateFormat;
 import java.util.Date;
+import java.util.Calendar;
 
-public class AddSessionActivity extends AppCompatActivity {
+public class AddSessionActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private TextView textSessionName;
+    private TextView textSessionDate;
+    private TextView textSessionTime;
+    private TextView textSessionLocation;
     private Button btnAddSession;
 
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference sessionsReference;
+    private Date    selectedDate;
+
+    private DatePickerDialog datePicker;
+    private TimePickerDialog timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,7 @@ public class AddSessionActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        selectedDate = new Date();
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         sessionsReference = firebaseDatabase.getReference().child("sessions");
@@ -35,14 +49,48 @@ public class AddSessionActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         textSessionName = (TextView)findViewById(R.id.textSessionName);
+        textSessionDate = (TextView)findViewById(R.id.txtDate);
+        textSessionTime = (TextView)findViewById(R.id.txtTime);
+        textSessionLocation = (TextView)findViewById(R.id.editLocation);
         btnAddSession = (Button)findViewById(R.id.btnAddSession);
+
+
+        final Calendar c = Calendar.getInstance();
+        c.setTime(selectedDate);
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int min = c.get(Calendar.MINUTE);
+
+        datePicker = new DatePickerDialog(
+                this, AddSessionActivity.this, year, month, day);
+        timePicker = new TimePickerDialog(
+                this, AddSessionActivity.this, hour, min, true);
+        refreshDateTimeFields();
+
+        textSessionDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker.show();
+            }
+        });
+
+        textSessionTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePicker.show();
+            }
+        });
 
         btnAddSession.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Session addSession = new Session();
+
+
                 addSession.setStartTime(new Date());
-                addSession.setLocation("Koornang Park");
+                addSession.setLocation(textSessionLocation.getText().toString());
                 addSession.setName(textSessionName.getText().toString());
 
                 DatabaseReference newSession = sessionsReference.push();
@@ -54,4 +102,32 @@ public class AddSessionActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        final Calendar c = Calendar.getInstance();
+        c.setTime(selectedDate);
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        selectedDate = c.getTime();
+
+        refreshDateTimeFields();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        final Calendar c = Calendar.getInstance();
+        c.setTime(selectedDate);
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        selectedDate = c.getTime();
+
+        refreshDateTimeFields();
+    }
+
+    protected void refreshDateTimeFields ()
+    {
+        textSessionDate.setText(DateFormat.getDateInstance(DateFormat.LONG).format(selectedDate));
+        textSessionTime.setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(selectedDate));
+    }
 }
--- app/src/main/java/com/frisbeeworld/drills/SessionAdapter.java
+++ app/src/main/java/com/frisbeeworld/drills/SessionAdapter.java
@@ -2,6 +2,9 @@ package com.frisbeeworld.drills;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -9,7 +12,6 @@ import android.widget.TextView;
 
 import com.frisbeeworld.drills.database.Session;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -33,19 +35,29 @@ public class SessionAdapter extends ArrayAdapter<Session> {
         TextView sessionDuration = (TextView)convertView.findViewById(R.id.textSessionDuration);
         TextView sessionLocation = (TextView)convertView.findViewById(R.id.textSessionLocation);
 
-        Session session = getItem(position);
+        final Session session = getItem(position);
 
-        teamName.setText("Caulfield Bears U8s");
+        teamName.setText("Caulfield Bears");
         sessionName.setText(session.getName());
-        Date startTime = session.getStartTime();
-        String startTimeStr = "";
-        if (startTime != null)
-        {
-            startTimeStr = startTime.toString();
-        }
-        sessionDateTime.setText(startTimeStr);
+        sessionDateTime.setText(session.getStartTimeString());
         sessionDuration.setText(Integer.toString(session.getDuration()));
         sessionLocation.setText(session.getLocation());
+        sessionLocation.setPaintFlags(
+                sessionLocation.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG
+        );
+        sessionLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri.Builder builder = new Uri.Builder();
+                Uri geoLoc = builder.scheme("geo").path("0,0").appendQueryParameter("q",
+                        session.getLocation()).build();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLoc);
+                if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+                    getContext().startActivity(intent);
+                }
+            }
+        });
 
         return convertView;
     }
